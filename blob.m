272a53aa1de5d732e50a6e3da1f4c5366b76fc55
visetu <165579359+visetu@users.noreply.github.com>
2025-09-09 18:15:23 -0500

    Update blob.m

--- blob.m
+++ blob.m
@@ -10,3 +10,29 @@ img(row_start:row_start+blob_size-1, col_start:col_start+blob_size-1) = ...
 
 subplot(2,4,1); imshow(uint8(img),[0 M-1 + blob_amp]);title('img');colormap('gray');colorbar;
 
+
+
+
+
+
+% Video writer setup
+v = VideoWriter('sensor_movie.avi');   % Create AVI file
+v.FrameRate = 2;                       % Slow enough to see changes
+open(v);
+
+for frame = 1:4
+  % Optionally, modify image per frame (demo: vary noise slightly)
+    noisy_img = img + randn(size(img)) * frame * A_noise/2;
+%   frame_to_write = uint8(255 * mat2gray(noisy_img));       % Normalize to [0,255]
+    grayscale_frame_to_write = uint8(255 * mat2gray(img));   % Normalize to [0,255]
+%    rgb_frame = ind2rgb(frame_to_write, jet(256));           % Convert to RGB for color
+%   writeVideo(v, rgb_frame);
+    writeVideo(v, grayscale_frame_to_write);
+end
+
+close(v); % Finalize and close video
+
+
+
+
+
